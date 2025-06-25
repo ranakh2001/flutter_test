eac88bd12133c8a039522c3e972bc7a151b59450
Rana El-khodary <ranaelkhodary2@gmail.com>
2025-06-25 12:10:56 +0300

    change ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.example.flutter_test1"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
-        targetSdk = 23
+        minSdk = 23
+        targetSdk = 33
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
